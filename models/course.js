0bfbf7f118ed72d344333cc1177eab77e6cb8096
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-14 15:27:47 +0100

    create model

---
+++ models/course.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+let course_schema = new mongoose.Schema({
+    title : String,
+    author : String,
+    tags : [String],
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    isPublished: Boolean,
+    price: Number
+});
+
+let Course = mongoose.model('Course',course_schema);
+
+module.exports.Course = Course;
