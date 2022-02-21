c6809b535d8c0c15bb31cdfe7bf9175b4ff99379
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-21 13:37:10 +0100

    msg

--- routers/courses.js
+++ routers/courses.js
@@ -8,10 +8,82 @@ router.post('',async (req,res)=>{
         course = await course.save();
         res.send(course);
     } catch (error) {
-        console.log('Error saving course :',error.message);
+        res.status(400).send('Error saving course :',error.message);
     }
     
 });
+
+// get All courses
+router.get('',async (req,res)=>{
+    try {
+        let courses = await Course.find();
+        res.send(courses)
+    } catch (error) {
+        res.status(400).send('Error saving course :',error.message);
+    }
+    
+});
+// get all course with only title and author
+router.get('/title/:t',async (req,res)=>{
+    try {
+        let courses = await Course.find({title : req.params.t},'title author -_id')
+                                    .sort({title : -1, price : 1})
+                                    //.limit(1)
+
+
+        res.send(courses)
+    } catch (error) {
+        res.status(400).send('Error saving course :',error.message);
+    }
+    
+})
+
+router.get('/price/over/:p',async (req,res)=>{
+    try {
+        let courses = await Course.find({price : { $gte : req.params.p}})
+
+
+        res.send(courses)
+    } catch (error) {
+        res.status(400).send('Error saving course :',error.message);
+    }
+    
+})
+
+
+router.get('/price/under/:p',async (req,res)=>{
+    try {
+        let courses = await Course.find({price : { $lte : req.params.p}})
+
+
+        res.send(courses)
+    } catch (error) {
+        res.status(400).send('Error saving course :',error.message);
+    }
+    
+})
+
+router.get('/title/in',async (req,res)=>{
+    try {
+        let courses = await Course.find({isPublished : { $in : [true]}})
+
+        res.send(courses)
+    } catch (error) {
+        res.status(400).send('Error saving course :',error.message);
+    }
+    
+})
+
+router.get('/title/starts/:prefixe',async (req,res)=>{
+    try {
+        let courses = await Course.find({title : new RegExp('^'+req.params.prefixe,'i')})
+
+        res.send(courses)
+    } catch (error) {
+        res.status(400).send('Error saving course :',error.message);
+    }
+    
+})
 
 
 module.exports=router;
