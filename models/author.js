38f54b1f2cb45f4ebdb15560cb64a9d841dd948c
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-21 15:18:16 +0100

    add author collection

--- models/author.js
+++ models/author.js
@@ -1,5 +1,26 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
 
+let author_schema = new mongoose.Schema({
+    name : String,
+    email : String,
+    telephone : String 
+});
 
-/*
-    author : Name , email , telephone
-*/
+let validation_author = Joi.object({
+    name : Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    telephone : Joi.string()
+});
+
+let validation_update_author = Joi.object({
+    name : Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    telephone : Joi.string()
+});
+
+let Author = mongoose.model('Author',author_schema);
+
+module.exports.Author = Author;
+module.exports.validation_author = validation_author;
+module.exports.validation_update_author = validation_update_author;
